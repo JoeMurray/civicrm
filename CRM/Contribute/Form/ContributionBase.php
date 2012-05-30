5d309c0c06f4c205e7a8a7173cfcdab5fb3d8577
eileen <eileen@mcnaughty.com>
2012-05-30 23:29:48 +1200

    CRM-5869 same as checkbox

--- CRM/Contribute/Form/ContributionBase.php
+++ CRM/Contribute/Form/ContributionBase.php
@@ -184,6 +184,7 @@ class CRM_Contribute_Form_ContributionBase extends CRM_Core_Form {
 
   public $_action;
 
+    public $_profileAddressFields = array();
   /**
    * Function to set variables up before form is built
    *
@@ -743,7 +744,7 @@ class CRM_Contribute_Form_ContributionBase extends CRM_Core_Form {
             }
             $stateCountryMap[$index][$prefixName] = $key;
           }
-
+          CRM_Core_BAO_UFField::assignAddressField($key, $this->_profileAddressFields);
           if ($onBehalf) {
             if (!empty($fieldTypes) && in_array($field['field_type'], $fieldTypes)) {
               CRM_Core_BAO_UFGroup::buildProfile($this, $field, CRM_Profile_Form::MODE_CREATE,
@@ -765,7 +766,9 @@ class CRM_Contribute_Form_ContributionBase extends CRM_Core_Form {
         }
 
         $this->assign($name, $fields);
-
+      if(!empty( $this->_profileAddressFields)){
+          $this->assign('profileAddressFields',$this->_profileAddressFields);
+        }
         require_once 'CRM/Core/BAO/Address.php';
         CRM_Core_BAO_Address::addStateCountryMap($stateCountryMap);
 
