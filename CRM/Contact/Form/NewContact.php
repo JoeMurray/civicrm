9de6042d868ad5d949b80ae87b6560c38fa619b8
eileen <eileen@mcnaughty.com>
2012-06-11 11:47:19 +1200

    merge fix on NewContact

--- CRM/Contact/Form/NewContact.php
+++ CRM/Contact/Form/NewContact.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 4.1                                                |
@@ -33,23 +34,23 @@
  *
  */
 
+
 /**
  * This class build form elements for select exitsing or create new contact widget
  */
-class CRM_Contact_Form_NewContact {
-
-  /**
-   * Function used to build form element for new contact or select contact widget
-   *
-   * @param object   $form form object
-   * @param int      $blocNo by default it is one, except for address block where it is
-   *                 build for each block
-   * @param array    $extrProfiles extra profiles that should be included besides reserved
-   *
-   * @access public
-   *
-   * @return void
-   */
+class CRM_Contact_Form_NewContact
+{
+    /**
+     * Function used to build form element for new contact or select contact widget
+     *
+     * @param object   $form form object
+     * @param int      $blocNo by default it is one, except for address block where it is
+     *                 build for each block
+     * @param array    $extrProfiles extra profiles that should be included besides reserved
+     *
+     * @access public
+     * @return void
+     */
   function buildQuickForm(&$form, $blockNo = 1, $extraProfiles = NULL, $required = FALSE, $prefix = '') {
     require_once 'CRM/Core/BAO/UFGroup.php';
     // call to build contact autocomplete
@@ -59,20 +60,17 @@ class CRM_Contact_Form_NewContact {
 
     $form->add('text', "{$prefix}contact[{$blockNo}]", ts('Select Contact'), $attributes, $required);
     $form->addElement('hidden', "{$prefix}contact_select_id[{$blockNo}]");
-    if (CRM_Core_Permission::check('edit all contacts') ||
-      CRM_Core_Permission::check('add contacts')
-    ) {
+
+    if (CRM_Core_Permission::check('edit all contacts') || CRM_Core_Permission::check('add contacts')) {
       // build select for new contact
-      require_once 'CRM/Core/BAO/UFGroup.php';
       $contactProfiles = CRM_Core_BAO_UFGroup::getReservedProfiles('Contact', $extraProfiles);
       $form->add('select', "{$prefix}profiles[{$blockNo}]", ts('Create New Contact'), array(
-        array(
-          '' => ts('- create new contact -')) + $contactProfiles,
-        FALSE, array('onChange' => "if (this.value) {  newContact{$blockNo}( this.value, {$blockNo} );}")
-      );
+          '' => ts('- create new contact -'),
+        ) + $contactProfiles, FALSE, array(
+          'onChange' => "if (this.value) { newContact{$prefix}{$blockNo}( this.value, {$blockNo}, '{$prefix}' );}",
+        ));
     }
     $form->assign('blockNo', $blockNo);
     $form->assign('prefix', $prefix);
   }
 }
-
