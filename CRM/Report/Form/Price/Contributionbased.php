eca98fa7a127cf809a82e6a7292a21789c671c79
eileenmcnaughton <eileen@mcnaughty.com>
2012-08-24 17:17:03 +1200

    Update contribution based to include price fields

--- CRM/Report/Form/Price/Contributionbased.php
+++ CRM/Report/Form/Price/Contributionbased.php
@@ -44,18 +44,20 @@ class CRM_Report_Form_Price_Contributionbased extends CRM_Report_Form_Extended {
 
     function __construct( ) {
 
-        $this->_columns = $this->getContactColumns()
-                        + $this->getContributionColumns()
-                        + $this->getLineItemColumns()
-
-                        ;
-       parent::__construct( );
+      $this->_columns = $this->getContactColumns()
+      + $this->getContributionColumns()
+      + $this->getPriceFieldColumns()
+      + $this->getPriceFieldValueColumns()
+      + $this->getLineItemColumns();
+      parent::__construct( );
     }
 
     function fromClauses( ) {
       return array(
         'lineItem_from_contribution',
         'contact_from_contribution',
+        'priceFieldValue_from_lineItem',
+        'priceField_from_lineItem'
       );
     }
 }
